ef24e29bad884e8d3be715642eb15acc980bd77a
Rummenigge <rummenigge_888@hotmail.com>
2014-08-12 23:22:16 -0300

    Pequenos ajustes na tela de login 23:22 12/08/2014

--- src/main/java/br/labican/ufrn/sabia/testes/Teste.java
+++ src/main/java/br/labican/ufrn/sabia/testes/Teste.java
@@ -1,6 +1,13 @@
 
 package br.labican.ufrn.sabia.testes;
 
+import br.labican.ufrn.sabia.dao.AutorizacaoJpaController;
+import br.labican.ufrn.sabia.dao.UsuarioJpaController;
+import br.labican.ufrn.sabia.modelo.Autorizacao;
+import br.labican.ufrn.sabia.modelo.Usuario;
+import br.labican.ufrn.sabia.util.Util;
+import java.util.List;
+
 /**
  *
  * @author Rummenigge Maia
@@ -8,17 +15,17 @@ package br.labican.ufrn.sabia.testes;
 public class Teste {
     
     public static void main(String[] asd) {
-//        AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
-//        List<Autorizacao> as = ajc.findAutorizacaoEntities();
-//        
-//        Usuario u = new Usuario();
-//        u.setUsername("Rummenigge");
-//        u.setPassword("asd123");
-//        u.setAtivo(true);        
-//        u.setAutorizacoes(as);
-//        
-//        UsuarioJpaController ujc = new UsuarioJpaController(Util.EMF);
-//        ujc.create(u);
+        AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
+        List<Autorizacao> as = ajc.findAutorizacaoEntities();
+        
+        Usuario u = new Usuario();
+        u.setUsername("Rummenigge");
+        u.setPassword("asd123");
+        u.setAtivo(true);        
+        u.setAutorizacoes(as);
+        
+        UsuarioJpaController ujc = new UsuarioJpaController(Util.EMF);
+        ujc.create(u);
 //        Autorizacao a1 = new Autorizacao(null, "ROLE_ADMIN");
 //        Autorizacao a2 = new Autorizacao(null, "ROLE_AVALI");
 //        Autorizacao a3 = new Autorizacao(null, "ROLE_OPERA");
