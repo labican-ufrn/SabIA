a779c377317a1acb5d4fe4bcca50976d8bf36fc3
Hyago Brendoll <brendollhyago@gmail.com>
2014-08-14 14:21:03 -0300

    Atualização da dependencia para o Postgresql 9.3

--- src/main/java/br/labican/ufrn/sabia/testes/Teste.java
+++ src/main/java/br/labican/ufrn/sabia/testes/Teste.java
@@ -26,12 +26,13 @@ public class Teste {
         
         UsuarioJpaController ujc = new UsuarioJpaController(Util.EMF);
         ujc.create(u);
-//        Autorizacao a1 = new Autorizacao(null, "ROLE_ADMIN");
-//        Autorizacao a2 = new Autorizacao(null, "ROLE_AVALI");
-//        Autorizacao a3 = new Autorizacao(null, "ROLE_OPERA");
-//        AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
-//        ajc.create(a1);
-//        ajc.create(a2);
-//        ajc.create(a3);
+        
+        /*Autorizacao a1 = new Autorizacao(null, "ROLE_ADMIN");
+        Autorizacao a2 = new Autorizacao(null, "ROLE_AVALI");
+        Autorizacao a3 = new Autorizacao(null, "ROLE_OPERA");
+        AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
+        ajc.create(a1);
+        ajc.create(a2);
+        ajc.create(a3);*/
     }
 }
