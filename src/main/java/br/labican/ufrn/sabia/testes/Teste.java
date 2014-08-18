3b02f06d9ce8378d92c816a414d1a59e6664e848
pablopes <pablo.infolopes@gmail.com>
2014-08-18 15:16:27 -0300

    mais atualizado

--- src/main/java/br/labican/ufrn/sabia/testes/Teste.java
+++ src/main/java/br/labican/ufrn/sabia/testes/Teste.java
@@ -1,11 +1,12 @@
 package br.labican.ufrn.sabia.testes;
+
 import java.util.List;
+
 import br.labican.ufrn.sabia.dao.AutorizacaoJpaController;
 import br.labican.ufrn.sabia.dao.UsuarioJpaController;
 import br.labican.ufrn.sabia.modelo.Autorizacao;
 import br.labican.ufrn.sabia.modelo.Usuario;
 import br.labican.ufrn.sabia.util.Util;
-
 
 /**
  *
@@ -13,26 +14,26 @@ import br.labican.ufrn.sabia.util.Util;
  */
 public class Teste {
 
-
 	public static void main(String[] asd) {
-		 AutorizacaoJpaController ajc = new
-		 AutorizacaoJpaController(Util.EMF);
-		 List<Autorizacao> as = ajc.findAutorizacaoEntities();
-		//
-		 Usuario u = new Usuario();
-		 u.setUsername("pablo");
-		 u.setPassword("123");
-		 u.setAtivo(true);
-		 u.setAutorizacoes(as);
-		
-		 UsuarioJpaController ujc = new UsuarioJpaController(Util.EMF);
-		 ujc.create(u);
-		/**Autorizacao a1 = new Autorizacao(null, "ROLE_ADMIN");	
-		Autorizacao a2 = new Autorizacao(null, "ROLE_AVALI");
-		Autorizacao a3 = new Autorizacao(null, "ROLE_OPERA");
-		AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
-		ajc.create(a1);
-		ajc.create(a2);
-		ajc.create(a3);**/
+//		AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
+//		List<Autorizacao> as = ajc.findAutorizacaoEntities();
+//
+//		Usuario u = new Usuario();
+//		u.setUsername("pablo");
+//		u.setPassword("123");
+//		u.setAtivo(true);
+//		u.setAutorizacoes(as);
+//
+//		UsuarioJpaController ujc = new UsuarioJpaController(Util.EMF);
+//		ujc.create(u);
+//		System.out.println("Usuário cadastrado com sucesso!");
+//		Autorizacao a1 = new Autorizacao(null, "ROLE_ADMIN");
+//		Autorizacao a2 = new Autorizacao(null, "ROLE_AVALI");
+//		Autorizacao a3 = new Autorizacao(null, "ROLE_OPERA");
+//		AutorizacaoJpaController ajc = new AutorizacaoJpaController(Util.EMF);
+//		ajc.create(a1);
+//		ajc.create(a2);
+//		ajc.create(a3);
+//		System.out.println("Autoizações cadastradas com sucesso!");
 	}
 }
