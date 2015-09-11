e9ca5ab6a0d200b982251a2be53482fe3f7de9c7
Hyago Brendoll <brendollhyago@gmail.com>
2015-09-11 19:58:19 -0300

    Add classe Main para observar execução.

---
+++ src/main/java/org/labican/sabia/main/Main.java
@@ -0,0 +1,38 @@
+package org.labican.sabia.main;
+
+import java.util.List;
+import org.labican.sabia.dao.JpaController;
+import org.labican.sabia.modelo.Macrorregiao;
+import org.labican.sabia.util.JPAUtil;
+
+/**
+ *
+ * @author hyago
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+//        Macrorregiao m = new Macrorregiao();
+//        JpaController dao = new JpaController(JPAUtil.EMF, Macrorregiao.class);
+//        
+//        m.setCodigoIBGE(2);
+//        m.setNome("Hyago2");
+//        
+//        dao.criar(m);
+//        
+//        m.setCodigoIBGE(8);
+//        m.setNome("Hyago8");
+//    
+//        dao.editar(m);
+//        dao.excluir(3);
+//        
+//        List<Macrorregiao> lista;
+//        lista = dao.pesquisarTodos();
+//        
+//        System.out.print("********* lista ****** \n");
+//        
+//        for(Macrorregiao l: lista){
+//            System.out.print("IBGE: " + l.getCodigoIBGE() + " - Nome: " + l.getNome() + "\n");
+//        } 
+    }
+}
